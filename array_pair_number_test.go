896aea1425a760183483295301b3e6f302aeeb49
Lucas.zhang <zhangliang925@icloud.com>
2021-02-16 22:48:39 +0800

    :sparkles: Add array pair number

---
+++ array_pair_number_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sort"
+	"testing"
+)
+
+// 561
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	result := 0
+	for i := 0; i < len(nums); i += 2 {
+		result += nums[i]
+	}
+	return result
+}
+
+var tests_561 = []struct {
+	input  []int
+	output int
+}{
+	{
+		input:  []int{1, 4, 3, 2},
+		output: 4,
+	},
+	{
+		input:  []int{6, 2, 6, 5, 1, 2},
+		output: 9,
+	},
+}
+
+func TestArrayPairSum(t *testing.T) {
+	for _, v := range tests_561 {
+		assert.Equal(t, arrayPairSum(v.input), v.output)
+	}
+}
