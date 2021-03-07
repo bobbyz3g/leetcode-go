8d9d837baccdf282cb0d51e6d039951ac758d436
Lucas.zhang <zhangliang925@icloud.com>
2021-03-07 22:59:05 +0800

    :sparkles: Add 131

---
+++ splitpalindromestr_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func partition(s string) [][]string {
+	l := len(s)
+
+	// flags of str[i, j] is palindrome string or not.
+	// f(i, j) = true when  i >= j
+	// f(i, j) = f(i+1, j-1) && str[i] == str[j] when i < j
+	pArr := make([][]bool, l)
+
+	// initial pArr
+	for i := range pArr {
+		pArr[i] = make([]bool, l)
+		for j := range pArr[i] {
+			pArr[i][j] = true
+		}
+	}
+
+	// check f(i, j)
+	for i := l - 1; i >= 0; i-- {
+		for j := i + 1; j < l; j++ {
+			pArr[i][j] = s[i] == s[j] && pArr[i+1][j-1]
+		}
+	}
+
+	result := make([][]string, 0, 1)
+	splits := []string{}
+
+	// dfs will be used in closures, you need to declare first
+	var dfs func(int)
+
+	dfs = func(i int) {
+		// no more elements, ends recursion
+		if i == l {
+			result = append(result, append([]string(nil), splits...))
+			return
+		}
+		for j := i; j < l; j++ {
+			if pArr[i][j] {
+				splits = append(splits, s[i:j+1])
+				dfs(j + 1)
+				splits = splits[:len(splits)-1]
+			}
+		}
+	}
+
+	dfs(0)
+
+	return result
+}
+
+var tests_131 = []struct {
+	s   string
+	out [][]string
+}{
+	{
+		s:   "aab",
+		out: [][]string{{"a", "a", "b"}, {"aa", "b"}},
+	},
+	{
+		s:   "a",
+		out: [][]string{{"a"}},
+	},
+}
+
+func TestPartition(t *testing.T) {
+	for _, v := range tests_131 {
+		assert.Equal(t, v.out, partition(v.s))
+	}
+}
