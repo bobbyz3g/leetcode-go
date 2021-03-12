2c38c58d744c93e69f45fc6fef95edfb0c7ad821
Lucas.zhang <zhangliang925@icloud.com>
2021-03-12 23:27:34 +0800

    :sparkles: Add 331.

---
+++ validserialization_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func isValidSerialization(preorder string) bool {
+	diff := 1
+	for _, c := range strings.Split(preorder, ",") {
+		diff -= 1
+		if diff < 0 {
+			return false
+		}
+		if c != "#" {
+			diff += 2
+		}
+	}
+	return diff == 0
+}
+
+var tests_331 = []struct {
+	preorder string
+	ok       bool
+}{
+	{
+		"9,3,4,#,#,1,#,#,2,#,6,#,#",
+		true,
+	},
+	{
+		"1,#",
+		false,
+	},
+	{
+		"9,#,#,1",
+		false,
+	},
+	{
+		"#",
+		true,
+	},
+}
+
+func TestIsValidSerialization(t *testing.T) {
+	for _, v := range tests_331 {
+		assert.Equal(t, v.ok, isValidSerialization(v.preorder))
+	}
+}
