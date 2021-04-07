a0c50602dd0ca9ed4e544b2ac3e17d81ea97922c
Zhang <zhangliang925@icloud.com>
2021-04-07 21:39:53 +0800

    :sparkles: Add ExistInRotatedSorted

--- array/array.go
+++ array/array.go
@@ -102,3 +102,47 @@ func RemoveDuplicates(nums []int, appearNum int) int {
 
 	return slow + 1
 }
+
+// ExistInRotatedSorted returns true if target is in nums,
+// or false if it is not in nums.
+//
+// nums sorted in non-decreasing order (not necessarily with distinct values).
+// and it is rotated at an unknown pivot index k (0 <= k < nums.length)
+// such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1],
+// nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+// For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5
+// and become [4,5,6,6,7,0,1,2,4,4].
+func ExistInRotatedSorted(nums []int, target int) bool {
+	n := len(nums)
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		return nums[0] == target
+	}
+
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[l] == nums[mid] && nums[mid] == nums[r] {
+			l++
+			r--
+		} else if nums[l] <= nums[mid] {
+			if nums[l] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[n-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return false
+}
