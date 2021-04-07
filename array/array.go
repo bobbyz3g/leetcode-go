8097295ae2e5e3704677f217b5c4c52fab308566
Zhang <zhangliang925@icloud.com>
2021-04-07 22:00:29 +0800

    :sparkles: Add SearchInRotatedSorted and refactor ExistInRotatedSorted.

--- array/array.go
+++ array/array.go
@@ -114,35 +114,56 @@ func RemoveDuplicates(nums []int, appearNum int) int {
 // and become [4,5,6,6,7,0,1,2,4,4].
 func ExistInRotatedSorted(nums []int, target int) bool {
 	n := len(nums)
+	f := func(i int) int {
+		return nums[i] - target
+	}
+	cmp := func(i, j int) int {
+		return nums[i] - nums[j]
+	}
+	index := SearchInRotatedSorted(n, f, cmp)
+	return index >= 0
+}
+
+// SearchInRotatedSorted returns index if f(index) == 0.
+// or -1 if it is not in array.
+// f(index) return 0, if array[i] equal to target.
+// cmp compares array[i] array[j], if array[i] equal array[j] returns 0,
+// if array[i] less than array[j] return -1, else return 1.
+func SearchInRotatedSorted(n int, f func(i int) int, cmp func(i, j int) int) int {
 	if n == 0 {
-		return false
+		return -1
 	}
 	if n == 1 {
-		return nums[0] == target
+		if f(0) == 0 {
+			return 0
+		}
+		return -1
+
 	}
 
 	l, r := 0, n-1
 	for l <= r {
-		mid := (l + r) / 2
-		if nums[mid] == target {
-			return true
+		mid := int((l + r) >> 1)
+		if f(mid) == 0 {
+			return mid
 		}
-		if nums[l] == nums[mid] && nums[mid] == nums[r] {
+		if cmp(l, mid) == 0 && cmp(mid, r) == 0 {
 			l++
 			r--
-		} else if nums[l] <= nums[mid] {
-			if nums[l] <= target && target < nums[mid] {
+		} else if cmp(l, r) <= 0 {
+			if f(l) <= 0 && f(mid) >= 0 {
 				r = mid - 1
 			} else {
 				l = mid + 1
 			}
 		} else {
-			if nums[mid] < target && target <= nums[n-1] {
+			if f(mid) < 0 && f(n-1) >= 0 {
 				l = mid + 1
 			} else {
 				r = mid - 1
 			}
 		}
 	}
-	return false
+
+	return -1
 }
