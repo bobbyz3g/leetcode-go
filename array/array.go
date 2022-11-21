2cc601d7e18a2bef0508e5f5a9fdcb89d075e6e6
Kaiser925 <zhangliang925@icloud.com>
2022-11-21 21:24:03 +0800

    feat: add 88 merge order array

--- array/array.go
+++ array/array.go
@@ -379,3 +379,24 @@ func maxAreaOfIslandDfs(grid [][]int, i, j int) int {
 	}
 	return res
 }
+
+// MergeOrderArray merges nums1 and nums2 into a single array
+// sorted in non-decreasing order.
+// The final sorted array should not be returned by the function,
+// but instead be stored inside the array nums1.
+// To accommodate this, nums1 has a length of m + n,
+// where the first m elements denote the elements that should be merged,
+// and the last n elements are set to 0 and should be ignored.
+// nums2 has a length of n.
+func MergeOrderArray(nums1 []int, m int, nums2 []int, n int) {
+	a, b := m-1, n-1
+	for i := m + n - 1; b >= 0; i-- {
+		if a >= 0 && nums1[a] > nums2[b] {
+			nums1[i] = nums1[a]
+			a--
+		} else {
+			nums1[i] = nums2[b]
+			b--
+		}
+	}
+}
