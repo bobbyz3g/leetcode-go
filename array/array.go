6504a667f8c788ce03984599ef93f61c00804680
Kaiser925 <zhangliang925@icloud.com>
2023-09-10 21:28:12 +0800

    :sparkles: add MaxSubarraySum

--- array/array.go
+++ array/array.go
@@ -811,3 +811,23 @@ func NextPermutation(nums []int) {
 		nums[i], nums[j] = nums[j], nums[i]
 	}
 }
+
+// MaxSubarraySum returns max sum of subarray.
+func MaxSubarraySum(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] >= 0 {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+	}
+	res := dp[0]
+	for i := 1; i < len(dp); i++ {
+		if dp[i] > res {
+			res = dp[i]
+		}
+	}
+	return res
+}
--- array/array_test.go
+++ array/array_test.go
@@ -1171,3 +1171,35 @@ func TestNextPermutation(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxSubarraySum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}},
+			want: 6,
+		},
+		{
+			name: "case2",
+			args: args{nums: []int{1}},
+			want: 1,
+		},
+		{
+			name: "case3",
+			args: args{nums: []int{5, 4, -1, 7, 8}},
+			want: 23,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, MaxSubarraySum(tt.args.nums), "MaxSubarraySum(%v)", tt.args.nums)
+		})
+	}
+}
