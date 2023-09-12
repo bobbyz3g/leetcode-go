713c95bbf825bbfa33d257c52002d2de26378eab
Kaiser925 <zhangliang925@icloud.com>
2023-09-12 22:09:27 +0800

    :sparkles: add InsertAndMergeOverlapping

--- array/array.go
+++ array/array.go
@@ -848,3 +848,26 @@ func RepairCars(ranks []int, cars int) int64 {
 		return s >= cars
 	}))
 }
+
+// InsertAndMergeOverlapping inserts newInterval into intervals,
+// than merge overlapping interval.
+func InsertAndMergeOverlapping(intervals [][]int, newInterval []int) [][]int {
+	res := make([][]int, 0)
+	n := len(intervals)
+	i := 0
+	for i < n && intervals[i][1] < newInterval[0] {
+		res = append(res, intervals[i])
+		i++
+	}
+	for i < n && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(newInterval[0], intervals[i][0])
+		newInterval[1] = max(newInterval[1], intervals[i][1])
+		i++
+	}
+	res = append(res, newInterval)
+	for i < n {
+		res = append(res, intervals[i])
+		i++
+	}
+	return res
+}
--- array/array_test.go
+++ array/array_test.go
@@ -1237,3 +1237,61 @@ func TestRepairCars(t *testing.T) {
 		})
 	}
 }
+
+func TestInsertAndMergeOverlapping(t *testing.T) {
+	type args struct {
+		intervals   [][]int
+		newInterval []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "case1",
+			args: args{
+				intervals:   [][]int{{1, 3}, {6, 9}},
+				newInterval: []int{2, 5},
+			},
+			want: [][]int{{1, 5}, {6, 9}},
+		},
+		{
+			name: "case2",
+			args: args{
+				intervals:   [][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}},
+				newInterval: []int{4, 8},
+			},
+			want: [][]int{{1, 2}, {3, 10}, {12, 16}},
+		},
+		{
+			name: "case3",
+			args: args{
+				intervals:   [][]int{},
+				newInterval: []int{5, 7},
+			},
+			want: [][]int{{5, 7}},
+		},
+		{
+			name: "case4",
+			args: args{
+				intervals:   [][]int{{1, 5}},
+				newInterval: []int{2, 3},
+			},
+			want: [][]int{{1, 5}},
+		},
+		{
+			name: "case5",
+			args: args{
+				intervals:   [][]int{{1, 5}},
+				newInterval: []int{2, 7},
+			},
+			want: [][]int{{1, 7}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, InsertAndMergeOverlapping(tt.args.intervals, tt.args.newInterval), "InsertAndMergeOverlapping(%v, %v)", tt.args.intervals, tt.args.newInterval)
+		})
+	}
+}
