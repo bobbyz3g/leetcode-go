421263cafc7b24453902dbfab673cf6f8c4795c3
Kaiser925 <zhangliang925@icloud.com>
2023-10-31 22:42:09 +0800

    :sparkles: add SearchRotated

--- array/array.go
+++ array/array.go
@@ -929,3 +929,47 @@ func Intersection(nums1 []int, nums2 []int) []int {
 
 	return res
 }
+
+// SearchRotated returns index of target. If
+// target is not in array, it will return -1.
+// Nums is possibly rotated at an unknown pivot
+// index k (1 <= k < nums.length) such that
+// the resulting array is [nums[k], nums[k+1], ...,
+// nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+func SearchRotated(nums []int, target int) int {
+	n := len(nums)
+	switch n {
+	case 0:
+		return -1
+	case 1:
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) >> 1
+		if nums[mid] == target {
+			return mid
+		}
+		// left is ordered
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+			continue
+		}
+
+		// right is ordered
+		if nums[mid] < target && target <= nums[n-1] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
+}
--- array/array_test.go
+++ array/array_test.go
@@ -1403,3 +1403,45 @@ func TestIntersection(t *testing.T) {
 		})
 	}
 }
+
+func TestSearchRotated(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{
+				nums:   []int{4, 5, 6, 7, 0, 1, 2},
+				target: 0,
+			},
+			want: 4,
+		},
+		{
+			name: "case2",
+			args: args{
+				nums:   []int{4, 5, 6, 7, 0, 1, 2},
+				target: 3,
+			},
+			want: -1,
+		},
+		{
+			name: "case3",
+			args: args{
+				nums:   []int{1},
+				target: 0,
+			},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, SearchRotated(tt.args.nums, tt.args.target), "SearchRotated(%v, %v)", tt.args.nums, tt.args.target)
+		})
+	}
+}
